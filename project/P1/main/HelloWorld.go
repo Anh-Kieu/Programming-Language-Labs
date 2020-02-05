546056dcd3734c7f7f1eadee20fefbc5df7dd551
Anh-Kieu <aqk001@bucknell.edu>
2020-02-05 17:17:29 -0500

    Phase 1 completed

---
+++ project/P1/main/HelloWorld.go
@@ -0,0 +1,15 @@
+/***
+ * Anh Kieu
+ * CSCI 208 - Prof. Wittie
+ * Phase 1 - Hello World
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World, Hello Go")
+}
+
+// Compiled in Windows. Compiled command: go HelloWorld.go. File must be in a main folder.
